47b4d0870115f6df72d7387b5398d859874be4e6
Ghaith Thamer <ghaith.th@gmail.com>
2024-09-18 04:10:49 +0300

    fix deployment some errors

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button';
 function FavoriteToggleButton({ productId }: { productId: string }) {
   return (
     <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
+      {productId}
       <FaHeart />
     </Button>
   );
