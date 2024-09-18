aa91122215f767ecf66e08717ec2b15d990a51ba
Ghaith Thamer <ghaith.th@gmail.com>
2024-09-18 03:45:26 +0300

    first commit

---
+++ app/products/loading.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import LoadingContainer from '@/components/global/LoadingContainer';
+
+function loading() {
+  return <LoadingContainer />;
+}
+export default loading;
+
+//ما عرفت شنو فايدة هاذا الكومبوننت مع العلم تم انشاءه في فيديو رقم 591
