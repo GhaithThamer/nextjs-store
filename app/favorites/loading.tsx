d8539a0ad40499cbd3f514520a35fe6e000d1215
Ghaith Thamer <ghaith.th@gmail.com>
2024-09-24 10:41:24 +0300

    reached to react share

---
+++ app/favorites/loading.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import LoadingContainer from '@/components/global/LoadingContainer';
+
+function loading() {
+  return <LoadingContainer />;
+}
+export default loading;
+
+//ليش سويناها كلاينت كومبوننت درس 629
